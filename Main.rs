76ef7bfbd414097a14d8e9bc7f6731624c412106
mpzinke <mathew.zinke@hotmail.com>
2022-03-20 21:04:28 -0500

    Initial commit

---
+++ Main.rs
@@ -0,0 +1,36 @@
+
+/***********************************************************************************************************************
+*                                                                                                                      *
+*   created by: MPZinke                                                                                                *
+*   on 2022.03.20                                                                                                      *
+*                                                                                                                      *
+*   DESCRIPTION: TEMPLATE                                                                                              *
+*   BUGS:                                                                                                              *
+*   FUTURE:                                                                                                            *
+*                                                                                                                      *
+***********************************************************************************************************************/
+
+
+static HUB_URL: &str = env!("HUB_URL");
+static API_KEY: &str = env!("API_KEY");
+
+
+static DAY_TIME_VALUE: &str = "\"ct\": 335";  // White
+static NIGHT_TIME_VALUE: &str = "\"xy\": [0.6867,0.3119]";  // Red
+
+
+
+fn make_request(value: String) -> bool
+{
+	return false;
+}
+
+
+
+fn main()
+{
+	while(true)
+	{
+		break;
+	}
+}
